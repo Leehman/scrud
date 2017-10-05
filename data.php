7e4e5d116a761c176c5c1aa0f3a56b657a04f7eb
lee <lh.man@aol.com>
2017-10-05 11:18:43 -0400

    convert to PDO

--- data.php
+++ data.php
@@ -33,44 +33,56 @@ $mysql_data = array();
 if ($job != ''){
 
   // Connect to database
-  $db_connection = mysqli_connect($db_server, $db_username, $db_password, $db_name);
-  if (mysqli_connect_errno()){
-    $result  = 'error';
-    $message = 'Failed to connect to database: ' . mysqli_connect_error();
-    $job     = '';
-  }
+  
+    //try {
+        $dsn = "mysql:host=$db_server;dbname=$db_name;charset=utf8";
+        $opt = [
+          PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+          PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+          PDO::ATTR_EMULATE_PREPARES   => false
+        ];
+        
+        $pdo = new PDO($dsn, $db_username, $db_password, $opt);
 
+    //} catch(PDOException $ex) {
+    //    echo 'ERROR: ' . $ex->getMessage();
+    //}
+ 
   // Execute job
   if ($job == 'get_companies'){
 
     // Get companies
-    $query = "SELECT * FROM it_companies ORDER BY rank";
-    $query = mysqli_query($db_connection, $query);
-    if (!$query){
-      $result  = 'error';
-      $message = 'query error';
-    } else {
-      $result  = 'success';
-      $message = 'query success';
-      while ($company = mysqli_fetch_array($query)){
-        $functions  = '<div class="function_buttons"><ul>';
-        $functions .= '<li class="function_edit"><a data-id="'   . $company['company_id'] . '" data-name="' . $company['company_name'] . '"><span>Edit</span></a></li>';
-        $functions .= '<li class="function_delete"><a data-id="' . $company['company_id'] . '" data-name="' . $company['company_name'] . '"><span>Delete</span></a></li>';
-        $functions .= '</ul></div>';
-        $mysql_data[] = array(
-          "rank"          => $company['rank'],
-          "company_name"  => $company['company_name'],
-          "industries"    => $company['industries'],
-          "revenue"       => '$ ' . $company['revenue'],
-          "fiscal_year"   => $company['fiscal_year'],
-          "employees"     => number_format($company['employees'], 0, '.', ','),
-          "market_cap"    => '$ ' . $company['market_cap'],
-          "headquarters"  => $company['headquarters'],
-          "functions"     => $functions
-        );
-      }
-    }
+      try{
+          $stmt = $pdo->query("SELECT * FROM it_companies ORDER BY rank ");
+          $result  = 'success';
+          $message = 'query success';
 
+          foreach ($stmt as $row)
+          { 
+              $functions  = '<div class="function_buttons"><ul>';
+              $functions .= '<li class="function_edit"><a data-id="'   . $row['company_id'] . '" data-name="' . $row['company_name'] . '"><span>Edit</span></a></li>';
+              $functions .= '<li class="function_delete"><a data-id="' . $row['company_id'] . '" data-name="' . $row['company_name'] . '"><span>Delete</span></a></li>';
+              $functions .= '</ul></div>';
+              //echo $row['rank'];
+              $mysql_data[] = array(
+                "rank"          => $row['rank'],
+                "company_name"  => $row['company_name'],
+                "industries"    => $row['industries'],
+                "revenue"       => '$ ' . $row['revenue'],
+                "fiscal_year"   => $row['fiscal_year'],
+                "employees"     => number_format($row['employees'], 0, '.', ','),
+                "market_cap"    => '$ ' . $row['market_cap'],
+                "headquarters"  => $row['headquarters'],
+                "functions"     => $functions
+            );
+          }
+      }
+      catch(PDOException $ex){
+        $result  = 'error';
+        $message = 'query error';
+        //echo 'ERROR: ' . $ex->getMessage();
+      }
+   
   } elseif ($job == 'get_company'){
 
     // Get company
@@ -78,49 +90,47 @@ if ($job != ''){
       $result  = 'error';
       $message = 'id missing';
     } else {
-      $query = "SELECT * FROM it_companies WHERE company_id = '" . mysqli_real_escape_string($db_connection, $id) . "'";
-      $query = mysqli_query($db_connection, $query);
-      if (!$query){
-        $result  = 'error';
-        $message = 'query error';
-      } else {
+      
+        $stmt = $pdo->prepare("SELECT * FROM it_companies WHERE company_id = ? ");
+        $stmt->execute([$id]);
+       
         $result  = 'success';
         $message = 'query success';
-        while ($company = mysqli_fetch_array($query)){
-          $mysql_data[] = array(
-            "rank"          => $company['rank'],
-            "company_name"  => $company['company_name'],
-            "industries"    => $company['industries'],
-            "revenue"       => $company['revenue'],
-            "fiscal_year"   => $company['fiscal_year'],
-            "employees"     => $company['employees'],
-            "market_cap"    => $company['market_cap'],
-            "headquarters"  => $company['headquarters']
-          );
+        foreach ($stmt as $row)
+        { $mysql_data[] = array(
+            "rank"          => $row['rank'],
+            "company_name"  => $row['company_name'],
+            "industries"    => $row['industries'],
+            "revenue"       => $row['revenue'],
+            "fiscal_year"   => $row['fiscal_year'],
+            "employees"     => $row['employees'],
+            "market_cap"    => $row['market_cap'],
+            "headquarters"  => $row['headquarters']
+           );
         }
-      }
+     
     }
 
   } elseif ($job == 'add_company'){
 
     // Add company
-    $query = "INSERT INTO it_companies SET ";
-    if (isset($_GET['rank']))         { $query .= "rank         = '" . mysqli_real_escape_string($db_connection, $_GET['rank'])         . "', "; }
-    if (isset($_GET['company_name'])) { $query .= "company_name = '" . mysqli_real_escape_string($db_connection, $_GET['company_name']) . "', "; }
-    if (isset($_GET['industries']))   { $query .= "industries   = '" . mysqli_real_escape_string($db_connection, $_GET['industries'])   . "', "; }
-    if (isset($_GET['revenue']))      { $query .= "revenue      = '" . mysqli_real_escape_string($db_connection, $_GET['revenue'])      . "', "; }
-    if (isset($_GET['fiscal_year']))  { $query .= "fiscal_year  = '" . mysqli_real_escape_string($db_connection, $_GET['fiscal_year'])  . "', "; }
-    if (isset($_GET['employees']))    { $query .= "employees    = '" . mysqli_real_escape_string($db_connection, $_GET['employees'])    . "', "; }
-    if (isset($_GET['market_cap']))   { $query .= "market_cap   = '" . mysqli_real_escape_string($db_connection, $_GET['market_cap'])   . "', "; }
-    if (isset($_GET['headquarters'])) { $query .= "headquarters = '" . mysqli_real_escape_string($db_connection, $_GET['headquarters']) . "'";   }
-    $query = mysqli_query($db_connection, $query);
-    if (!$query){
-      $result  = 'error';
-      $message = 'query error';
-    } else {
-      $result  = 'success';
-      $message = 'query success';
-    }
+
+    $stmt = $pdo->prepare("INSERT INTO it_companies (rank, company_name, industries, revenue, fiscal_year, employees, market_cap, headquarters) 
+              VALUES (:rank, :company_name, :industries, :revenue, :fiscal_year, :employees, :market_cap, :headquarters)");
+    
+    $stmt->bindParam(':rank', $_GET['rank']);
+    $stmt->bindParam(':company_name', $_GET['company_name']);
+    $stmt->bindParam(':industries', $_GET['industries']);
+    $stmt->bindParam(':revenue', $_GET['revenue']);
+    $stmt->bindParam(':fiscal_year', $_GET['fiscal_year']);
+    $stmt->bindParam(':employees', $_GET['employees']);
+    $stmt->bindParam(':market_cap', $_GET['market_cap']);
+    $stmt->bindParam(':headquarters', $_GET['headquarters']);
+  
+    $stmt->execute();
+  
+    $result  = 'success';
+    $message = 'query success';
 
   } elseif ($job == 'edit_company'){
 
@@ -129,24 +139,25 @@ if ($job != ''){
       $result  = 'error';
       $message = 'id missing';
     } else {
-      $query = "UPDATE it_companies SET ";
-      if (isset($_GET['rank']))         { $query .= "rank         = '" . mysqli_real_escape_string($db_connection, $_GET['rank'])         . "', "; }
-      if (isset($_GET['company_name'])) { $query .= "company_name = '" . mysqli_real_escape_string($db_connection, $_GET['company_name']) . "', "; }
-      if (isset($_GET['industries']))   { $query .= "industries   = '" . mysqli_real_escape_string($db_connection, $_GET['industries'])   . "', "; }
-      if (isset($_GET['revenue']))      { $query .= "revenue      = '" . mysqli_real_escape_string($db_connection, $_GET['revenue'])      . "', "; }
-      if (isset($_GET['fiscal_year']))  { $query .= "fiscal_year  = '" . mysqli_real_escape_string($db_connection, $_GET['fiscal_year'])  . "', "; }
-      if (isset($_GET['employees']))    { $query .= "employees    = '" . mysqli_real_escape_string($db_connection, $_GET['employees'])    . "', "; }
-      if (isset($_GET['market_cap']))   { $query .= "market_cap   = '" . mysqli_real_escape_string($db_connection, $_GET['market_cap'])   . "', "; }
-      if (isset($_GET['headquarters'])) { $query .= "headquarters = '" . mysqli_real_escape_string($db_connection, $_GET['headquarters']) . "'";   }
-      $query .= "WHERE company_id = '" . mysqli_real_escape_string($db_connection, $id) . "'";
-      $query  = mysqli_query($db_connection, $query);
-      if (!$query){
-        $result  = 'error';
-        $message = 'query error';
-      } else {
-        $result  = 'success';
-        $message = 'query success';
-      }
+      $stmt = $pdo->prepare("UPDATE it_companies SET rank = :rank, company_name = :company_name, industries = :industries
+                                , revenue = :revenue, fiscal_year = :fiscal_year, employees = :employees, market_cap = :market_cap
+                                , headquarters = :headquarters 
+                                WHERE company_id = :id") ;
+      $stmt->bindParam(':rank', $_GET['rank']);
+      $stmt->bindParam(':company_name', $_GET['company_name']);
+      $stmt->bindParam(':industries', $_GET['industries']);
+      $stmt->bindParam(':revenue', $_GET['revenue']);
+      $stmt->bindParam(':fiscal_year', $_GET['fiscal_year']);
+      $stmt->bindParam(':employees', $_GET['employees']);
+      $stmt->bindParam(':market_cap', $_GET['market_cap']);
+      $stmt->bindParam(':headquarters', $_GET['headquarters']);
+      $stmt->bindParam(':id', $id);
+      $stmt->execute();
+  
+      $result  = 'success';
+      $message = 'query success';
+
+
     }
 
   } elseif ($job == 'delete_company'){
@@ -156,21 +167,25 @@ if ($job != ''){
       $result  = 'error';
       $message = 'id missing';
     } else {
-      $query = "DELETE FROM it_companies WHERE company_id = '" . mysqli_real_escape_string($db_connection, $id) . "'";
-      $query = mysqli_query($db_connection, $query);
-      if (!$query){
-        $result  = 'error';
-        $message = 'query error';
-      } else {
+      $stmt = $pdo->prepare("DELETE FROM it_companies WHERE company_id = ?");
+      $stmt->execute([$id]);
+      $deleted = $stmt->rowCount();
+      
+      if($deleted > 0){
         $result  = 'success';
         $message = 'query success';
+      }else{
+        $result  = 'error';
+        $message = 'bad DB call';
       }
+      
+  
     }
 
   }
 
   // Close database connection
-  mysqli_close($db_connection);
+  $pdo = null;
 
 }
 
